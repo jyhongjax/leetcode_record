b36d7c962e87988f2eb372f5f43ffe411bbc1c3d
jyhong <jyhongjax@outlook.com>
2020-01-11 11:21:56 +0800

    Add files via upload

---
+++ 70_Climbing_Stairs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution 
+{
+public:
+    int climbStairs(int n) 
+    {
+        vector<int> dp(n + 1, 1);
+        for (int i = 2; i <= n; ++i)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+    Solution s;
+    cout << s.climbStairs(n) << endl;
+    return 0;
+}
